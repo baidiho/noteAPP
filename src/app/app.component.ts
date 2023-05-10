d4beb14987172114adaef5e0d0daaf6ba4b627e2
Ihor <ygrik10@gmail.com>
2023-05-10 21:25:59 +0300

    Test with matUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import User from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements User {
   title = 'todoapp';
 }
